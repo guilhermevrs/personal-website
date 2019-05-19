72c51ef5902dd35559429d506da208f8c3008ef6
guilhermevrs <guilhermevrs@gmail.com>
2019-05-19 19:32:01 +0200

    Fixes 404 text

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -5,8 +5,7 @@ const NotFound = () => (
     <div className={styles['not-found']}>
         <h1 className={styles['not-found__title']}>Oops!</h1>
         <p className={styles['not-found__body']}>
-        I couldn't find the article your looking for.<br/>
-        Perhaps take a look at the latest ones below
+        I couldn't find the article your looking for. Below you can find the latest ones.
         </p>
     </div>
 );
